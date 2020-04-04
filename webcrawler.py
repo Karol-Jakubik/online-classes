dff3ccf4050982f06eda278699742a0343a9c2ed
Karol-Jakubik <61844514+Karol-Jakubik@users.noreply.github.com>
2020-04-04 16:20:05 +0200

    get details(header) for every page

--- webcrawler.py
+++ webcrawler.py
@@ -11,26 +11,39 @@ import urllib.parse
 
 
 def main():
-    
     print('Getting staff urls ...')
 
     staff_url='http://wa.amu.edu.pl/wa/en/staff_list'
     staff_content = get_content(staff_url)
     
-    links = staff_content.find_all('a')
-    
+    links = staff_content.find_all('a')    
+
     urls = []
+
     for link in links:
         if len(link.get_text()) > 1:
             base_url = 'http://wa.amu.edu.pl'
             url= urllib.parse.urljoin(base_url, link['href'])
             urls.append(url)
-        
+    
+    print('Staff header found:')    
+    for url in urls:
+        print(get_details(url))
+
     print('Urls found:')
     print('\n'.join(urls))
+
+
 def get_content(url):
     response = urllib.request.urlopen(url)
     data= response.read()
     doc = BeautifulSoup(data, 'html.parser')
     return doc.find(id = 'tresc_wlasciwa')
+
+
+def get_details(url):
+    content = get_content(url)
+    header = content.find('h1')
+    return header.get_text()
+
 main()
